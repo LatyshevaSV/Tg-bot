3dc521314e1f2c15e852f3b5051eaea931e7122d
LatyshevaS <latisheva_sveta@inbox.ru>
2025-04-27 21:44:55 +0300

    Второй коммит: Инициализация бота.

---
+++ main.py
@@ -0,0 +1,8 @@
+from aiogram import Bot, Dispatcher, types
+
+# Токен бота
+BOT_TOKEN = "7619827644:AAG7b9njo-8LqqzsqQX1n0t-Af4J5CROyAg"
+
+# Инициализация бота и диспетчера
+bot = Bot(token=BOT_TOKEN)
+dp = Dispatcher()
